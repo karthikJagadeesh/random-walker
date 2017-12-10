039fbb9b2ca6c1673773a9f7952c2c021e616b06
Karthik Jagadeesha <kartu8@gmail.com>
2017-12-10 19:28:12 +0530

    Add walker mirroring

--- src/index.js
+++ src/index.js
@@ -3,19 +3,28 @@ import p5 from "p5";
 import { Walker } from "./walker";
 
 const app = p => {
-  let walker = null;
+  let walker1 = null;
+  let walker2 = null;
+  let screenCenter = 0;
 
   p.setup = _ => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.background(255);
-    walker = new Walker(p);
-    walker.show();
+    screenCenter = p.width / 2
+    walker1 = new Walker(p, [191, 53, 47]);
+    walker2 = new Walker(p, [27, 72, 105]);
+    walker1.show();
+    walker2.show();
   };
 
   p.draw = _ => {
-    walker.move();
-    walker.show();
-    // p.background(255);
+    walker1.move();
+    let offSetValue = screenCenter - walker1.x;
+    let mirrorValue = screenCenter + offSetValue;
+    walker2.x = mirrorValue;
+    walker2.y = walker1.y;
+    walker1.show();
+    walker2.show();
   }
 
   p.windowResized = _ => {
--- src/walker.js
+++ src/walker.js
@@ -1,32 +1,40 @@
 class Walker {
-  constructor(p) {
+  constructor(p, color) {
     this._p = p;
     this.x = this._p.width / 2;
     this.y = this._p.height / 2;
+    this.color = color;
   }
 
   show() {
     const p = this._p;
 
-    p.fill(0);
+    p.noStroke();
+    p.fill(this.color);
     p.ellipse(this.x, this.y, 2, 2);
   }
 
   move() {
     const p = this._p;
 
-    const choice = Math.floor(p.random(4));
+    const choice = Math.floor(p.random(10));
     switch (choice) {
       case 0:
-        this.x = this.x + 2;
-        break;
       case 1:
+      case 2:
+      case 3:
+        this.y = this.y + 1;
+        break;
+      case 4:
+      case 5:
         this.x = this.x - 2;
         break;
-      case 2:
-        this.y = this.y + 2;
+      case 6:
+      case 7:
+        this.x = this.x + 2;
         break;
-      case 3:
+      case 8:
+      case 9:
         this.y = this.y - 2;
         break;
     }
